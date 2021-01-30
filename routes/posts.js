39278d413576a3242cb2b6f53eeb9c16e8c43bf5
Akash Kumar Pandey <akashakypandey@gmail.com>
2021-01-30 13:44:43 +0530

    Feature of deleting posts and deleting comment has been added. Post can be deleted by the author only and Comment may be deleted by author or by commenter

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,5 @@ const postController = require('../controller/posts_controller');
 
 
 router.post('/create', passport.checkAuthentication, postController.create);
-
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 module.exports = router;  
